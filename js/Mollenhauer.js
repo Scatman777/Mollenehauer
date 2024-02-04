e7a87a97a548ff136152f7e67d9ba40ce13e847e
hostr777 <divakovbogdan11@gmail.com>
2024-02-04 18:14:41 +0300

    Update

--- js/Mollenhauer.js
+++ js/Mollenhauer.js
@@ -14,13 +14,13 @@
 
 // const normalizeUrl = (site) => {
 //     let normalizedUrl;
-  
+
 //     if (site.startsWith('https://')) {
 //       normalizedUrl = site;
 //     } else {
 //       normalizedUrl = `https://${site}`;
 //     }
-  
+
 //     return normalizedUrl;
 // }
 
@@ -30,7 +30,7 @@
 //     if (text === '') {
 //       return '';d
 //     }
-  
+
 //     const reversable = text[0] !== text[0].toUpperCase();
 //     return reversable ? reverse(text) : text;
 //   };
@@ -38,15 +38,15 @@
 // convertText('');
 
 // const getNumberExplanation = () => {
-  // if (666 === 'devil number') {
-  //   console.log('devil number');
-  // } else if (42 ==='answer for everything') {
-  //   console.log('answer for everything');
-  // } else if (7 === 'prime number') {
-  //   console.log('prime number');
-  // } else {
-  //   console.log('error')
-  // }
+// if (666 === 'devil number') {
+//   console.log('devil number');
+// } else if (42 ==='answer for everything') {
+//   console.log('answer for everything');
+// } else if (7 === 'prime number') {
+//   console.log('prime number');
+// } else {
+//   console.log('error')
+// }
 
 //   switch (getNumberExplanation) {
 //     case 666:
@@ -65,15 +65,48 @@
 
 // getNumberExplanation();
 
-const printNumbers = (initialNumber = 4) => {
-  let i = 1;
+// const printNumbers = (initialNumber = 4) => {
+//   let i = 1;
 
-  while (i <= initialNumber) {
-    console.log(i);
-    i = i - 1;
-  }
+//   while (i <= initialNumber) {
+//     console.log(i);
+//     i = i - 1;
+//   }
 
-  console.log('finished!');
-};
+//   console.log('finished!');
+// };
 
-printNumbers();
+// printNumbers();
+
+// const countChars = (str, char) => {
+//   let i = 0;
+//   let count = 0;
+//   while (i < str.length) {
+//     if (str[i] === char) {
+//       // Считаем только подходящие символы
+//       count = count + 1;
+//     }
+//     // Счетчик увеличивается в любом случае
+//     i = i + 1;
+//   }
+
+//   console.log(count);
+// };
+
+// countChars("hellLoee", "l");
+
+// const makeItFunny = (str, n) => {
+//   let i = 0;
+//   let result = "";
+//   while (i < str.length) {
+//     const current = str[i];
+//     if ((i + 1) % n === 0) {
+//       result = `${result}${current.toUpperCase()}`;
+//     } else {
+//       result = `${result}${current}`;
+//     }
+//     i++;
+//   }
+
+//   return result;
+// };
